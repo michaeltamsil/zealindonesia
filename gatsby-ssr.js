2ddf1c0504f1fec933c1707c881fb1ccb534dbc3
michael tamsil <michaeltamsil@gmail.com>
2019-05-10 17:59:51 +0700

    update icon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,8 +4,10 @@ import Footer from './src/components/organisms/Footer'
 export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
     
     setHeadComponents([
+        <title>Zeal</title>,
         <link rel="shortcut icon" href="/favicon.png"/>,
         <link href="https://fonts.googleapis.com/css?family=Roboto:900" rel="stylesheet"/>,
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"></link>,
         <link rel="stylesheet" href='/bootstrap.min.css' />
     ])
 
--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default () => {
     return (
-        <div>
-            email, instagram whatsap
+        <div className="text-center mt-4">
+            <i className="far fa-envelope fa-5x mr-4"></i>
+            <i className="fab fa-instagram fa-5x mr-4"></i>
+            <i className="fab fa-whatsapp fa-5x"></i>
         </div>
     )
 }
